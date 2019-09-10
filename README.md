ca14c87e0e7ac5639ba2fd1a9346066dbf660441
Ram Basnet <rambasnet@gmail.com>
2019-09-09 21:03:46 -0600

    added gitrepos.sh script

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# gitassignments
-scripts working with git assignments
+# git assignments
+
+scripts working with assignments as git repositories
+
+## Using this script
+
+1. create a list of repositiories for each class in username/Repo-Name format
+2. download gitrepos.sh script somewhere at your semester folder e.g., Fall2019
+3. to clone all the repositories run:
+
+    ```
+    $ ./gitrepos.sh clone repo.list
+    ```
+
+4. to pull new submissions run:
+
+    ```
+    $ ./gitrepos.sh pull Course/grading
+    ```
---
+++ gitrepos.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+cloneRepos() {
+    echo $1
+    repoFile=$1
+    DIR=$(dirname "${repoFile}")
+    if [ ! -d $DIR ]; then
+        mkdir -p $DIR/grading
+    fi
+    total=0
+    fail=0
+    while read repo; do
+        repoPath="git@github.com:$repo.git"
+        #echo "cloning $repoPath "
+        cd $DIR/grading > /dev/null 2>&1
+        #git clone $repoPath 
+        git clone $repoPath > git.log 2>&1
+        ((total+=1))
+        OUTPUT=$(egrep -i "ERROR:" git.log)
+        #echo $OUTPUT
+        if [ -n "$OUTPUT" ]
+        then
+            ((fail+=1))
+            echo "$OUTPUT: $repoPath"
+        fi
+    done < $repoFile
+    echo "Failed cloning $fail of $total repositotires!"
+    rm git.log
+}
+
+pullRepos() {
+    rootFolder=$1
+    for d in $(ls $rootFolder); do
+        repo="$rootFolder/$d"
+        echo "pulling $repo"
+        git -C $repo stash # stash local changes so won't affect pull
+        git -C $repo pull
+    done
+    echo "All done pulling repos for grading!"
+}
+
+if [[ $# < 2 ]]; then 
+    echo "Usage: ${0} command file or folder"
+    echo "comand = clone or pull"
+    echo "Example: ${0} clone <gitrepos.txt>"
+    echo "Example: ${0} pull <CS2/grading/>"
+    echo "Not enough arguments provided... Exiting"
+    exit 1
+fi
+
+if [[ $1 == "clone" ]]; then
+    echo "cloning repos from file $2"
+    cloneRepos $2
+elif [[ $1 == "pull" ]]; then
+    echo "pulling repos in root directory $2"
+    pullRepos $2
+else
+    echo "not a command!"
+fi
